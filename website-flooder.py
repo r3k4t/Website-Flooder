caa95ae14135140b14ce58e1f6ebeb30c24e9934
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-08-16 12:11:03 +0530

    Update website-flooder.py

--- website-flooder.py
+++ website-flooder.py
@@ -4,6 +4,8 @@ os.system("clear")
 os.system("figlet -f standard  Website Flooder")
 print
 print "Author  :  Rahat Khan Tusar(RKT)"
+print
+print "Github  : https://github.com/r3k4t "
 print 
 print "Information :  Use  vpn and tor network.Be anonymous.Be secure."
 print
